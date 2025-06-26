eb4cd5837e34dba58a7d8bf3ccfb8af63fc34d64
gvia <gritcaev.vv@gmail.com>
2025-06-26 19:39:39 +0300

    реализован CRUD для модели Client

--- mailings/views.py
+++ mailings/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView,DeleteView
+from .models import Client
 
-# Create your views here.
+
+class ClientListView(ListView):
+    model = Client
+    template_name = 'mailings/client_list.html'
+
+
+class ClientCreateView(CreateView):
+    model =Client
+    fields = ['email', 'full_name', 'comment']
+    template_name = 'mailings/client_from.html'
+    success_url = reverse_lazy('mailings:client_list')
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    fields = ['email', 'full_name', 'comment']
+    template_name = 'mailings/client_from.html'
+    success_url = reverse_lazy('mailings:client_list')
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    template_name = 'mailings/client_confirm_delete.html'
+    success_url = reverse_lazy('mailings:client_list')
